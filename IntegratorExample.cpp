a2bb56d08af27169d142cfb0210e4bbe12df11ad
janfo <mackos.nikos@interia.pl>
2025-01-13 19:58:24 +0100

    Added integral example

---
+++ IntegratorExample.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+#include "Integrator.h"
+
+#define NR_OF_DISC_PTS 1500
+
+
+
+int main()
+{
+
+
+	auto fun = std::function<float(float)>([=](float x)  -> float {return (x); });
+	Integrator <float, float> Integ(fun, NR_OF_DISC_PTS, { 0, 2});
+	std::cout<< "Integral value: "<< Integ.integrate() << std::endl;
+	 
+	
+	return 0;
+}
